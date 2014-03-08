edcbc0c36d73b8fd0ee15f28bc6d9e0f288f9ce4
carlosgsouza <carlosgsouza@gmail.com>
2014-03-07 22:49:29 -0300

    Implementing SpellCheckService

--- grails-app/services/net/carlosgsouza/spellchecker/SpellCheckService.groovy
+++ grails-app/services/net/carlosgsouza/spellchecker/SpellCheckService.groovy
@@ -1,10 +1,22 @@
 package net.carlosgsouza.spellchecker
 
-import org.xeustechnologies.googleapi.spelling.SpellChecker
-import org.xeustechnologies.googleapi.spelling.SpellResponse
+import com.swabunga.spell.engine.SpellDictionaryHashMap
+import com.swabunga.spell.event.SpellCheckEvent
+import com.swabunga.spell.event.SpellCheckListener
+import com.swabunga.spell.event.SpellChecker
+import com.swabunga.spell.event.StringWordTokenizer
+import com.swabunga.spell.event.TeXWordFinder
 
-class SpellCheckService {
+class SpellCheckService  {
+	
+	static SpellChecker spellChecker
+	static {
+		// This takes a long time to be initialized so we share it among all instances of the service
+		spellChecker = new SpellChecker(new SpellDictionaryHashMap(new File("dictionary/en.txt")))
+	}
 	
     int getErrorCount(String text) {
+		StringWordTokenizer tokenizer = new StringWordTokenizer(text, new TeXWordFinder());
+		return Math.max(0, spellChecker.checkSpelling(tokenizer))
     }
 }
--- test/unit/net/carlosgsouza/spellchecker/SpellCheckServiceSpec.groovy
+++ test/unit/net/carlosgsouza/spellchecker/SpellCheckServiceSpec.groovy
@@ -1,7 +1,26 @@
+
 package net.carlosgsouza.spellchecker
 
-import spock.lang.Specification;
+import spock.lang.Specification
 
 class SpellCheckServiceSpec extends Specification {
-
+	
+	SpellCheckService service = new SpellCheckService()
+	
+	def "should get the number of errors in a text"() {
+		expect:
+		service.getErrorCount(text) == errorCount
+		
+		where:
+		text				| errorCount
+		""					| 0
+		" "					| 0
+		" \t\n"				| 0
+		"Hello"				| 0
+		"hellow"			| 1
+		"hello world"		| 0
+		"hellow world"		| 1
+		"hellow wurld"		| 2
+		"helLO wOrLd"		| 0
+	}
 }
